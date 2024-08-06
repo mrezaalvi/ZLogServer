ec4a983b8055d0603e1be5cd8f8442275ad555d5
muhammad reza alvi <mrezaalvi@gmail.com>
2024-08-06 14:21:42 +0700

    create route and controller for authentication

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,15 +3,34 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(Request $request){
+        if(!Auth::attempt($request->only('email', 'password')))
+            return response()->json([
+                'message'=>'Unauthorized'], 401);
+        
+        $user = Auth::user();
 
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login success',
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
     }
 
     public function logout(){
+        Auth::user()->tokens()->delete();
 
+        return response()->json([
+            'message' => 'logout success',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\API\AuthController;
 
 Route::post('login',[AuthController::class,'login']);
 
-Route::post('logout',[AuthController::class,'logout']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Route::post('logout', [AuthController::class,'logout']);
+});
