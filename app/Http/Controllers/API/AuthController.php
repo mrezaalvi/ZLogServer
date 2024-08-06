320430477335da37a3749fef1ea251d3deb7d8d5
muhammad reza alvi <mrezaalvi@gmail.com>
2024-08-06 11:47:44 +0700

    Add AuthController, change config fortifyand jetstream

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+
+    }
+
+    public function logout(){
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+
+Route::post('login',[AuthController::class,'login']);
+
+Route::post('logout',[AuthController::class,'logout']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
