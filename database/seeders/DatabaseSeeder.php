bd3035427ac4cf3e665218482229a5ca316bfe7b
muhammad reza alvi <mrezaalvi@gmail.com>
2024-08-20 15:22:50 +0700

    modify: database/seeder/DatabaseSeeder.php fix to use Illuminate\Database\Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
