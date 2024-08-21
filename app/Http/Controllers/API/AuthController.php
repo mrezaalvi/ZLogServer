54acc21dbcb2fb7948b6b6295df935cc18802358
muhammad reza alvi <mrezaalvi@gmail.com>
2024-08-21 16:38:03 +0700

    create controller file: app/Http/Controllers/API/AuthControllers.php

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,34 +3,60 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     public function login(Request $request){
-        if(!Auth::attempt($request->only('email', 'password')))
-            return response()->json([
-                'message'=>'Unauthorized'], 401);
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            // 'device_name' => 'required',
+        ]);
+
+        // if(!Auth::attempt($request->only('email', 'password')) && $validated)
+        //     return response()->json(['message'=>'Unauthorized'], 401);
         
-        $user = Auth::user();
+        // $user = Auth::user();
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+        // $token = $user->createToken('auth_token')->plainTextToken;
 
+        $user = User::where('email', $request->email)->first();
+ 
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+     
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        
         return response()->json([
-            'message' => 'Login success',
-            'access_token' => $token,
-            'token_type' => 'Bearer'
+            'data'=>[
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ],
         ]);
     }
 
-    public function logout(){
-        Auth::user()->tokens()->delete();
+    public function logout(Request $request){
+        // Auth::user()->tokens()->delete();
+        $request->user()->currentAccessToken()->delete();
 
         return response()->json([
             'message' => 'logout success',
         ]);
+    }
+
+    public function getUser(Request $request){
+        return $request->user();
     }
 }
