6142718506a4b072e7e0f259ccbaaf3fab7a32a9
muhammad reza alvi <mrezaalvi@gmail.com>
2024-08-22 15:49:19 +0700

    modified:   app/Http/Controllers/UserManagementController.php

--- app/Http/Controllers/UserManagementController.php
+++ app/Http/Controllers/UserManagementController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 
 class UserManagementController extends Controller
 {
     public function index()
     {
-        return Inertia::render('UserManagement');
+        $users = User::whereNot(function(Builder $query){
+            $query->where('email','admin@example.com');
+        })->get();
+        return Inertia::render('UserManagement/Index', ['users' => $users]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('UserManagement/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $user = new User($request->all());
+        $user->save();
+        return redirect()->route('manajemen-pengguna.index');
+    }
+
+    public function destroy(User $manajemen_pengguna)
+    {
+        $manajemen_pengguna->delete();
+        return redirect()->back();
     }
 }
