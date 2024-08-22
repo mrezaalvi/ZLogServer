1ca6e189c63cfe4838180baf7c102b9315359d02
muhammad reza alvi <mrezaalvi@gmail.com>
2024-08-22 15:58:01 +0700

    modified: dependencies from node package manager

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,5 +20,8 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
 };
