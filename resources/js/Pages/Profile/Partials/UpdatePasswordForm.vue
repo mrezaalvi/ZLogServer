3f10706073d0e949dada5b1b1cf5370fd45ebd18
muhammad reza alvi <mrezaalvi@gmail.com>
2024-08-23 11:03:58 +0700

    resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -40,16 +40,16 @@ const updatePassword = () => {
 <template>
     <FormSection @submitted="updatePassword">
         <template #title>
-            Update Password
+            Perbarui Kata Sandi
         </template>
 
         <template #description>
-            Ensure your account is using a long, random password to stay secure.
+            Pastikan akun Anda menggunakan kata sandi yang panjang dan acak untuk tetap aman.
         </template>
 
         <template #form>
             <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="current_password" value="Current Password" />
+                <InputLabel for="current_password" value="Kata Sandi Saat Ini" />
                 <TextInput
                     id="current_password"
                     ref="currentPasswordInput"
@@ -62,7 +62,7 @@ const updatePassword = () => {
             </div>
 
             <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="password" value="New Password" />
+                <InputLabel for="password" value="Kata Sandi Baru" />
                 <TextInput
                     id="password"
                     ref="passwordInput"
@@ -75,7 +75,7 @@ const updatePassword = () => {
             </div>
 
             <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
+                <InputLabel for="password_confirmation" value="Konfirmasi Kata Sandi" />
                 <TextInput
                     id="password_confirmation"
                     v-model="form.password_confirmation"
@@ -89,11 +89,11 @@ const updatePassword = () => {
 
         <template #actions>
             <ActionMessage :on="form.recentlySuccessful" class="me-3">
-                Saved.
+                Tersimpan.
             </ActionMessage>
 
             <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Save
+                Simpan
             </PrimaryButton>
         </template>
     </FormSection>
