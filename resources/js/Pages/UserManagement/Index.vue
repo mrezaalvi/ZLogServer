07c993083d2e809a690751a3da6790769d06cfbd
muhammad reza alvi <mrezaalvi@gmail.com>
2024-08-23 16:19:06 +0700

    modified:   resources/js/Pages/UserManagement/Index.vue

--- resources/js/Pages/UserManagement/Index.vue
+++ resources/js/Pages/UserManagement/Index.vue
@@ -15,7 +15,6 @@
     const form = useForm({});
 
     const deleteUser = (user) => {
-        console.log(user)
         form.delete(route('manajemen-pengguna.destroy',user), {preserveScroll: true,});
     };
 </script>
@@ -58,11 +57,25 @@
 
                                     </td>
                                     <td class="w-px px-6 py-4 whitespace-nowrap">
-                                        <div class="flex space-x-4">
-                                            <a href="#" class="text-sm text-green-600 font-medium hover:text-green-700 hover:underline">
+                                        <div class="flex space-x-2">
+                                            <Link :href="`manajemen-pengguna/${user.id}/edit`" class="inline-flex items-center px-3 py-2.5  text-sm text-green-600 font-medium hover:text-green-700 hover:underline">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 mr-2 stroke-2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
+                                                </svg>
+
+                                                Ubah Kata Sandi
+                                            </Link>
+                                            <Link :href="`manajemen-pengguna/${user.id}/edit`" class="inline-flex items-center px-3 py-2.5  text-sm text-green-600 font-medium hover:text-green-700 hover:underline">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 mr-2 stroke-2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                </svg>
+
                                                 Edit
-                                            </a>
-                                            <button @click="deleteUser(user.id)" class="text-sm text-red-600 font-medium hover:text-red-700 hover:underline">
+                                            </Link>
+                                            <button @click="deleteUser(user.id)" class="inline-flex items-center px-3 py-2.5 text-sm text-red-600 font-medium hover:text-red-700 hover:underline">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 mr-2 stroke-2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                </svg>
                                                 Delete
                                             </button>
                                         </div>
