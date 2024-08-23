3610013a80d042a8ed7eca249ceb454e5c5a0a95
muhammad reza alvi <mrezaalvi@gmail.com>
2024-08-23 16:41:47 +0700

    modified:   resources/js/Pages/UserManagement/Index.vue

--- resources/js/Pages/UserManagement/Index.vue
+++ resources/js/Pages/UserManagement/Index.vue
@@ -1,6 +1,13 @@
 <script setup>
+    import { ref } from 'vue';
     import AppLayout from '@/Layouts/AppLayout.vue';
     import { useForm, Link } from "@inertiajs/vue3";
+    import DangerButton from '@/Components/DangerButton.vue';
+    import SecondaryButton from '@/Components/SecondaryButton.vue';
+    import DialogModal from '@/Components/DialogModal.vue';
+
+    
+    const confirmingUserDeletion = ref(false);
 
     defineProps({
         users: {
@@ -14,8 +21,21 @@
     
     const form = useForm({});
 
+    const confirmUserDeletion = () => {
+        confirmingUserDeletion.value = true;
+
+        setTimeout(() => passwordInput.value.focus(), 250);
+    };
+
+    const closeModal = () => {
+        confirmingUserDeletion.value = false;
+    };
+
     const deleteUser = (user) => {
-        form.delete(route('manajemen-pengguna.destroy',user), {preserveScroll: true,});
+        form.delete(route('manajemen-pengguna.destroy',user), {
+            preserveScroll: true,
+            onSuccess: () => closeModal(),
+        });
     };
 </script>
 
@@ -72,7 +92,8 @@
 
                                                 Edit
                                             </Link>
-                                            <button @click="deleteUser(user.id)" class="inline-flex items-center px-3 py-2.5 text-sm text-red-600 font-medium hover:text-red-700 hover:underline">
+                                            <!-- <button @click="deleteUser(user.id)" class="inline-flex items-center px-3 py-2.5 text-sm text-red-600 font-medium hover:text-red-700 hover:underline"> -->
+                                            <button @click="confirmUserDeletion" class="inline-flex items-center px-3 py-2.5 text-sm text-red-600 font-medium hover:text-red-700 hover:underline">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 mr-2 stroke-2">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                 </svg>
@@ -80,6 +101,31 @@
                                             </button>
                                         </div>
                                     </td>
+                                    <!-- Delete Account Confirmation Modal -->
+                                    <DialogModal :show="confirmingUserDeletion" @close="closeModal">
+                                        <template #title>
+                                            Hapus Data Pengguna
+                                        </template>
+
+                                        <template #content>
+                                            Apakah Anda yakin ingin menghapus data ini? Setelah akun dihapus, semua sumber daya dan data akan dihapus secara permanen. 
+                                        </template>
+
+                                        <template #footer>
+                                            <SecondaryButton @click="closeModal">
+                                                Cancel
+                                            </SecondaryButton>
+
+                                            <DangerButton
+                                                class="ms-3"
+                                                :class="{ 'opacity-25': form.processing }"
+                                                :disabled="form.processing"
+                                                @click="deleteUser(user.id)"
+                                            >
+                                                Delete Account
+                                            </DangerButton>
+                                        </template>
+                                    </DialogModal>
                                 </tr>
                                 
                             </tbody>
